5b4814e82f050a02ff34c7eb19736f8020a7e273
MUHAMMADYASEEN-Developer <muhammadyaseen692006@gmail.com>
2024-07-29 19:18:33 +0500

    integers

---
+++ integers.dart
@@ -0,0 +1,11 @@
+main() {
+  String Name = "Muhammad Yaseen";
+  int Age = 17;
+  int Class = 10;
+  double Percent = 76.0;
+  bool isPass = true;
+  print(" \n My name is $Name & I am $Age years old. \n ");
+  print(
+      "My Last Passed class was $Class & I score $Percent in $Class th Class \n ");
+  print("Did i  passed my last class ? $isPass\n");
+}
